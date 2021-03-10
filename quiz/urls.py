0d6b711e8e2fe74c2a1b3af43991c986f7eeebe7
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-10 21:27:48 +0530

    views and templates updated

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import *
 
 urlpatterns = [
-    
+    path('', index_page, name='index_page'),
+    path('<int:pk>/', take_quiz, name='take_quiz'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Category, Question
 # Create your views here.
+
+
+def index_page(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'index.html', context)
+
+
+def take_quiz(request, pk):
+    if request.method == 'GET':
+        questions = Question.objects.filter(choice=pk)
+        context = {'questions': questions}
+        return render(request, 'quiz.html', context)
+    
+    if request.method == 'POST':
+        pass
