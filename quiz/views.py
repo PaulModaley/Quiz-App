22020c2d741e485ae1290aba58b3bd246e16ba8c
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-11 15:33:24 +0530

    pagination added for quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Category, Question
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 
@@ -12,7 +14,10 @@ def index_page(request):
 def take_quiz(request, pk):
     if request.method == 'GET':
         questions = Question.objects.filter(choice=pk)
-        context = {'questions': questions}
+        paginator = Paginator(questions,1)
+        page_number = request.GET.get('page')
+        page_obj = Paginator.get_page(paginator, page_number)
+        context = {'questions': questions, 'page_obj': page_obj}
         return render(request, 'quiz.html', context)
     
     if request.method == 'POST':
