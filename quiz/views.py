de789870c99e3da2dcf6f453efe88d2d6b314fc7
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-12 00:45:17 +0530

    bug fixes

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Category, Question
-from django.core.paginator import Paginator
-
+from django.http import HttpResponse
+from django.contrib import messages
 # Create your views here.
 
 
@@ -12,13 +12,26 @@ def index_page(request):
 
 
 def take_quiz(request, pk):
+    questions = Question.objects.filter(choice=pk)
+    context = {'questions': questions, 'values': request.POST}
     if request.method == 'GET':
-        questions = Question.objects.filter(choice=pk)
-        paginator = Paginator(questions,1)
-        page_number = request.GET.get('page')
-        page_obj = Paginator.get_page(paginator, page_number)
-        context = {'questions': questions, 'page_obj': page_obj}
+        # paginator = Paginator(questions,1)
+        # page_number = request.GET.get('page')
+        # page_obj = Paginator.get_page(paginator, page_number)
         return render(request, 'quiz.html', context)
     
     if request.method == 'POST':
-        pass
+        answers = []
+        for i in questions:
+            answers.append(i.answer)
+        print(answers)
+        user_answer = request.POST['option']
+        # print(type(user_answer))
+        # context = {'questions': questions, 'user_answer': user_answer}
+        if user_answer in answers:
+            messages.success(request, 'Correct answer')
+            # return redirect('take_quiz')
+        else:
+            messages.error(request, 'Wrong answer')
+        return render(request, 'quiz.html', context)
+
